cdf31a7c82b92c35411ef1c079cd8d58c6202ede
Oleksiy Kudelsky <aleksiy.kudelsky@workday.com>
2019-04-13 12:40:55 -0700

    add calendar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Schedule/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Schedule from './components/Schedule.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Schedule
   }
 }
 </script>
---
+++ src/components/Schedule.vue
@@ -0,0 +1,21 @@
+<template>
+    <calendar style="height: 800px;"/>
+</template>
+
+<script>
+import 'tui-calendar/dist/tui-calendar.css'
+import { Calendar } from '@toast-ui/vue-calendar'
+
+export default {
+  name: 'Schedule',
+  components: {
+      'calendar': Calendar
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
