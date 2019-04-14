ccbe1e9423fbc3444a7c689a336ebbe3c5d7cc82
Winson Wong <winson.wong@workday.com>
2019-04-13 17:28:20 -0700

    added tabs on onbording

--- src/components/OnboardingModal.vue
+++ src/components/OnboardingModal.vue
@@ -3,89 +3,107 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container flex-row">
-
           <div class="modal-header">
-            <h2 name="header">
-              Preferences
-            </h2>
+            <h2 name="header">Preferences</h2>
           </div>
-
           <div class="modal-body">
-            <div class="item-spacing">Select your time zone and working hours.
-              <md-field>
-                <label>Select your day...</label>
-                <md-select v-model="onboardAvoidDay">
-                  <md-option value="1">Monday</md-option>
-                  <md-option value="2">Tuesday</md-option>
-                  <md-option value="3">Wednesday</md-option>
-                  <md-option value="4">Thursday</md-option>
-                  <md-option value="5">Friday</md-option>
-                  <md-option value="6">Saturday</md-option>
-                  <md-option value="7">Sunday</md-option>
-                </md-select>
-              </md-field>
-            </div>
-            <div class="item-spacing">Would you like your meetings to be clustered together or buffered?
-              <md-radio v-model="radio" name="onboardPrefClustered" value="clustered">Clustered</md-radio>
-              <md-radio v-model="radio" name="onboardPrefBuffered" value="buffered">Buffered</md-radio>
-              <div v-if="radio === 'buffered'">
-                <md-field>
-                  <label>Minute Intervals</label>
-                  <md-select v-model="bufferedIntervals">
-                    <md-option value="5">15 minutes</md-option>
-                    <md-option value="10">Tuesday</md-option>
-                    <md-option value="15">Wednesday</md-option>
-                    <md-option value="20">Thursday</md-option>
-                    <md-option value="25">Friday</md-option>
-                    <md-option value="30">Saturday</md-option>
-                    <md-option value="35">Sunday</md-option>
-                    <md-option value="40">Sunday</md-option>
-                    <md-option value="45">Sunday</md-option>
-                    <md-option value="50">Sunday</md-option>
-                    <md-option value="55">Sunday</md-option>
-                  </md-select>
-                </md-field>
-              </div>
-            </div>
-            <div class="item-spacing">What would be the day(s) that you would want to avoid having meetings?
-              <md-field>
-                <label>Select your day...</label>
-                <md-select v-model="onboardAvoidDay">
-                  <md-option value="1">Monday</md-option>
-                  <md-option value="2">Tuesday</md-option>
-                  <md-option value="3">Wednesday</md-option>
-                  <md-option value="4">Thursday</md-option>
-                  <md-option value="5">Friday</md-option>
-                  <md-option value="6">Saturday</md-option>
-                  <md-option value="7">Sunday</md-option>
-                </md-select>
-              </md-field>
-            </div>
-            <div class="item-spacing">What would be the day(s) that you are most likely available for meetings?
-              <md-field>
-                <label>Select your day...</label>
-                <md-select v-model="onboardPreferredDay">
-                  <md-option value="1">Monday</md-option>
-                  <md-option value="2">Tuesday</md-option>
-                  <md-option value="3">Wednesday</md-option>
-                  <md-option value="4">Thursday</md-option>
-                  <md-option value="5">Friday</md-option>
-                  <md-option value="6">Saturday</md-option>
-                  <md-option value="7">Sunday</md-option>
-                </md-select>
-              </md-field>
-            </div>
-            <div class="item-spacing" required>
-              Do you prefer your meetings in the morning or afternoon?
-              <md-radio v-model="radio" name="onboardPrefMorningAfternoon" value="morning">Morning</md-radio>
-              <md-radio v-model="radio" name="onboardPrefMorningAfternoon" value="afternoon">Afternoon</md-radio>
-            </div>
+            <md-tabs class="md-transparent" md-alignment="fixed">
+              <md-tab id="tab-home" md-label="1">
+                <div class="item-spacing">
+                  Select your time zone and working hours.
+                  <md-field>
+                    <label>Select your day...</label>
+                    <md-select v-model="onboardAvoidDay">
+                      <md-option value="1">Monday</md-option>
+                      <md-option value="2">Tuesday</md-option>
+                      <md-option value="3">Wednesday</md-option>
+                      <md-option value="4">Thursday</md-option>
+                      <md-option value="5">Friday</md-option>
+                      <md-option value="6">Saturday</md-option>
+                      <md-option value="7">Sunday</md-option>
+                    </md-select>
+                  </md-field>
+                </div>
+              </md-tab>
+              <md-tab id="tab-pages" md-label="2">
+                <div class="item-spacing">
+                  Would you like your meetings to be clustered together or buffered?
+                  <md-radio v-model="radio" name="onboardPrefClustered" value="clustered">Clustered</md-radio>
+                  <md-radio v-model="radio" name="onboardPrefBuffered" value="buffered">Buffered</md-radio>
+                  <div v-if="radio === 'buffered'">
+                    <md-field>
+                      <label>Minute Intervals</label>
+                      <md-select v-model="bufferedIntervals">
+                        <md-option value="5">15 minutes</md-option>
+                        <md-option value="10">Tuesday</md-option>
+                        <md-option value="15">Wednesday</md-option>
+                        <md-option value="20">Thursday</md-option>
+                        <md-option value="25">Friday</md-option>
+                        <md-option value="30">Saturday</md-option>
+                        <md-option value="35">Sunday</md-option>
+                        <md-option value="40">Sunday</md-option>
+                        <md-option value="45">Sunday</md-option>
+                        <md-option value="50">Sunday</md-option>
+                        <md-option value="55">Sunday</md-option>
+                      </md-select>
+                    </md-field>
+                  </div>
+                </div>
+              </md-tab>
+              <md-tab id="tab-posts" md-label="3">
+                <div class="item-spacing">
+                  What would be the day(s) that you would want to avoid having meetings?
+                  <md-field>
+                    <label>Select your day...</label>
+                    <md-select v-model="onboardAvoidDay">
+                      <md-option value="1">Monday</md-option>
+                      <md-option value="2">Tuesday</md-option>
+                      <md-option value="3">Wednesday</md-option>
+                      <md-option value="4">Thursday</md-option>
+                      <md-option value="5">Friday</md-option>
+                      <md-option value="6">Saturday</md-option>
+                      <md-option value="7">Sunday</md-option>
+                    </md-select>
+                  </md-field>
+                </div>
+              </md-tab>
+              <md-tab id="tab-favorites" md-label="4">
+                <div class="item-spacing">
+                  What would be the day(s) that you are most likely available for meetings?
+                  <md-field>
+                    <label>Select your day...</label>
+                    <md-select v-model="onboardPreferredDay">
+                      <md-option value="1">Monday</md-option>
+                      <md-option value="2">Tuesday</md-option>
+                      <md-option value="3">Wednesday</md-option>
+                      <md-option value="4">Thursday</md-option>
+                      <md-option value="5">Friday</md-option>
+                      <md-option value="6">Saturday</md-option>
+                      <md-option value="7">Sunday</md-option>
+                    </md-select>
+                  </md-field>
+                </div>
+              </md-tab>
+              <md-tab id="tab-favorites" md-label="5">
+                <div class="item-spacing" required>
+                  Do you prefer your meetings in the morning or afternoon?
+                  <md-radio
+                    v-model="radio"
+                    name="onboardPrefMorningAfternoon"
+                    value="morning"
+                  >Morning</md-radio>
+                  <md-radio
+                    v-model="radio"
+                    name="onboardPrefMorningAfternoon"
+                    value="afternoon"
+                  >Afternoon</md-radio>
+                </div>
+              </md-tab>
+            </md-tabs>
           </div>
 
           <div class="modal-footer">
-              <md-button class="md-dense md-raised md-primary margin-reset" @click="$emit('close')">
-                OK
-              </md-button>
+            <md-button class="md-dense md-raised md-primary margin-reset" @click="$emit('close')">OK</md-button>
           </div>
         </div>
       </div>
@@ -96,44 +114,43 @@
 
 <script>
 export default {
-  name: 'Modal',
-  props: {
-  },
+  name: "Modal",
+  props: {},
   methods: {
     close() {
-      this.$emit('close');
+      this.$emit("close");
     }
   },
   data: () => ({
-      radio: false
+    radio: false
   }),
   computed: {
-      onboardAvoidDay: {
-        get () {
-          return this.$material.locale.onboardAvoidDay
-        },
-        set (val) {
-          this.$material.locale.onboardAvoidDay = val
-        }
+    onboardAvoidDay: {
+      get() {
+        return this.$material.locale.onboardAvoidDay;
       },
-      onboardPreferredDay: {
-        get () {
-          return this.$material.locale.onboardPreferredDay
-        },
-        set (val) {
-          this.$material.locale.onboardPreferredDay = val
-        }
+      set(val) {
+        this.$material.locale.onboardAvoidDay = val;
+      }
+    },
+    onboardPreferredDay: {
+      get() {
+        return this.$material.locale.onboardPreferredDay;
       },
-      bufferedIntervals: {
-        get () {
-          return this.$material.locale.bufferedIntervals
-        },
-        set (val) {
-          this.$material.locale.bufferedIntervals = val
-        }
+      set(val) {
+        this.$material.locale.onboardPreferredDay = val;
+      }
+    },
+    bufferedIntervals: {
+      get() {
+        return this.$material.locale.bufferedIntervals;
+      },
+      set(val) {
+        this.$material.locale.bufferedIntervals = val;
       }
     }
-}
+  }
+};
 </script>
 
 <style scoped>
@@ -144,9 +161,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: table;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
@@ -160,8 +177,8 @@ export default {
   padding: 20px 30px;
   background-color: #fff;
   border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
   font-family: Helvetica, Arial, sans-serif;
   text-align: left !important;
 }
